bfd827c184acdd16b52250addeb550704e414a9b
DOMAYA <c2hila@gmail.com>
2022-11-21 21:20:10 +0900

    도현정 - 캘린더 서비스 초안 작성

--- src/main/java/kr/or/kosa/service/calendar/CalendarAllListService.java
+++ src/main/java/kr/or/kosa/service/calendar/CalendarAllListService.java
@@ -1,10 +1,15 @@
 package kr.or.kosa.service.calendar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.or.kosa.action.Action;
 import kr.or.kosa.action.ActionForward;
+import kr.or.kosa.dao.CalendarDao;
+import kr.or.kosa.dto.Calendar;
 
 public class CalendarAllListService implements Action {
 
@@ -12,11 +17,28 @@ public class CalendarAllListService implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward forward = new ActionForward();
 		
+		System.out.println("캘린더 서비스 진입");
+		
+		//List<Calendar> calendarList = new ArrayList<>();
+		List<Calendar> calendarList = null;
+		
+		//일정 받아와서 setAttribute	
+		
+		//DB작업
 		try {
+			CalendarDao dao = new CalendarDao();
+			calendarList = dao.CalendarAlllist();
+			
+			request.setAttribute("calendarList", calendarList);
 			
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		} 
+		
+		//TODO:뷰 설정하기
+		forward.setRedirect(false);
+		forward.setPath(null);
+		
 		return forward;
 	}
 
--- src/main/java/kr/or/kosa/service/calendar/CalendarUpdateService.java
+++ src/main/java/kr/or/kosa/service/calendar/CalendarUpdateService.java
@@ -1,10 +1,14 @@
 package kr.or.kosa.service.calendar;
 
+import java.text.SimpleDateFormat;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.or.kosa.action.Action;
 import kr.or.kosa.action.ActionForward;
+import kr.or.kosa.dao.CalendarDao;
+import kr.or.kosa.dto.Calendar;
 
 public class CalendarUpdateService implements Action {
 
@@ -12,11 +16,59 @@ public class CalendarUpdateService implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
 		ActionForward forward = new ActionForward();
 		
+		System.out.println("캘린더 수정 서비스 진입");
+		
+		String calendar_no = request.getParameter("calendar_no");
+		String calendar_start = request.getParameter("calendar_start");
+		String calendar_end = request.getParameter("calendar_end");
+		String calendar_content = request.getParameter("calendar_content");
+		String calendar_status = request.getParameter("calendar_status");
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD");
+		
+		String msg = "";
+		String url = "";
+		
 		try {
+			CalendarDao dao = new CalendarDao();
+			Calendar calendar = dao.CalendarDetail(Integer.parseInt(calendar_no));//이 함수로 가져오는거 맞지?
+			
+			//인덱스가 잘못됐을 경우
+			if(calendar_no == null || calendar_no.trim().equals("")) {
+				response.sendRedirect(""); //TODO:캘린더 목록으로 가도록
+				return null;
+			}
+			
+			//수정하려는 calendar가 null일 경우
+			if(calendar == null) {
+				msg = "캘린더 데이터 오류";
+				url = "";//TODO:캘린더 목록으로 가도록
+				
+				request.setAttribute("msg", msg);
+				request.setAttribute("url", url);
+				
+				forward.setRedirect(false);
+				forward.setPath(""); //TODO: redirect.jsp
+			}
+			
+			//수정사항 반영
+			calendar.setCalendar_start(formatter.parse(calendar_start));
+			calendar.setCalendar_end(formatter.parse(calendar_end));
+			calendar.setCalendar_content(calendar_content);
+			calendar.setCalendar_status(Integer.parseInt(calendar_status));
+			
+			int result = dao.CalendarUpdate(calendar);
+			
+			request.setAttribute("calendar", calendar);
+			
+			forward.setRedirect(false);
+			//TODO:뷰 지정
+			forward.setPath("");
 			
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		} 
+		
 		return forward;
 	}
 
