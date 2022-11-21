e71d9d2ab4958fbd00ebfe81509e12c1886e98bf
TaeWooKim <dhkdtpdn@gmail.com>
2022-11-21 14:11:53 +0900

    팝업공지사항 service 생성

---
+++ src/main/java/kr/or/kosa/popup/PopupLikeListService.java
@@ -0,0 +1,23 @@
+package kr.or.kosa.popup;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.kosa.action.Action;
+import kr.or.kosa.action.ActionForward;
+
+public class PopupLikeListService implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		ActionForward forward = new ActionForward();
+		
+		try {
+			
+		} catch (Exception e) {
+			
+		} 
+		return forward;
+	}
+
+}
